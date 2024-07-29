235d955b4e6ae472921d2d7d1a6d8abf51442a12
Bahman Moazzen <bahmanmoazzen@gmail.com>
2024-07-29 07:53:39 +0330

    Camera Manager Done

--- Assets/ImpossibleMaze3DV2/Scripts/Game/Camera/CameraManager.cs
+++ Assets/ImpossibleMaze3DV2/Scripts/Game/Camera/CameraManager.cs
@@ -8,14 +8,34 @@ public class CameraManager : MonoBehaviour
 {
     public static event UnityAction OnCamraSetup;
     [SerializeField] CinemachineVirtualCamera[] _gameCameras;
+
+    int _cameraIndex = 0;
     /// <summary>
     /// calls from other scrips to setup cameras
     /// </summary>
     /// <param name="iBallTransform">the transform of the ball in game</param>
     /// <param name="iLookAtTransform">the transform of the look at in game</param>
-    public void _SetupCameras(Transform iBallTransform,Transform iLookAtTransform)
+    public void _SetupCameras(Transform iBallTransform, Transform iLookAtTransform)
     {
-        
+        foreach (var cam in _gameCameras)
+        {
+            cam.Follow = iBallTransform;
+            cam.LookAt = iLookAtTransform;
+            cam.Priority = 0;
+        }
+        _gameCameras[_cameraIndex].Priority = 1;
+    }
+    public void _SwapCamera()
+    {
+        _gameCameras[_cameraIndex].Priority = 0;
+        _cameraIndex++;
+        if (_cameraIndex >= _gameCameras.Length)
+        {
+            _cameraIndex = 0;
+        }
+
+        _gameCameras[_cameraIndex].Priority = 1;
+
     }
 
 }
